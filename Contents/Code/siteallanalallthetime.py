d03beb158b23f74adc8f38a456d4c86c5d1d2ff7
ferengi82 <christian.maurer@gmx.eu>
2020-01-30 23:07:55 +0100

    second try

--- Contents/Code/siteallanalallthetime.py
+++ Contents/Code/siteallanalallthetime.py
@@ -16,29 +16,33 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     searchPageNum = 1
     while searchPageNum <= lastPage:
         searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + "/sd3.php?show=recent_video_updates&page=" + str(searchPageNum))
+        searchPostNum = 1
         for searchResult in searchResults.xpath('//div[@class="videoarea clear"]'):
+            PageNum = searchPageNum
+            Log("PageNum: " + str(PageNum))
+            PostNum = searchPostNum
+            Log("PostNum: " + str(searchPostNum))
             titleNoFormatting = searchResult.xpath('.//h3/a')[0].text_content().strip()
             Log("title:" + titleNoFormatting)
-            curID = searchResult.xpath('.//h3/a')[0].text_content().strip().replace('/','_').replace('?','!')
+
+            curID = (PAsearchSites.getSearchSearchURL(siteNum) + "/sd3.php?show=recent_video_updates&page=" + str(PageNum)).replace('/','+').replace('?','!') + "-" + str(PostNum)
+
             Log("curID: " + curID)
             releaseDate = parse(searchResult.xpath('.//p[@class="date"]')[0].text_content().strip()).strftime('%Y-%m-%d')
             Log("releaseDate: " + releaseDate)
+
             actorNames = searchResult.xpath('.//span[@style="color:#FF0000; font-weight:bold;"]')[0].text_content().strip()
             Log("actors: " + actorNames)
-            genres = searchResult.xpath('.//h4/a')[0].text_content().split(':')[1].strip()
-            Log("genres: " + genres)
-            summary = searchResult.xpath('.//div[@class="video_details clear"]/p')[0].text_content().split(':')[1][:-15].strip()
-            Log("summary: " + summary)
-            imageUrl = PAsearchSites.getSearchSearchURL(siteNum) + "/" + searchResult.xpath('.//div[@class="video_pic"]/a/img')[0].get("src").replace('/','_').replace('?','!')
-            Log("imageURL: " + imageUrl)
+            
             if searchDate:
                 score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
                 Log("score: " + str(score))
             else:
                 score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
                 Log("score: " + str(score))
-            results.Append(MetadataSearchResult(id = titleNoFormatting + "|" + releaseDate + "|" + actorNames + "|" + genres + "|" + summary + "|" + imageUrl , name = releaseDate + " - " + actorNames + " / " + titleNoFormatting + " [AllAnalAllTheTime] ", score = score, lang = lang))
+            results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum) , name = releaseDate + " - " + actorNames + " / " + titleNoFormatting + " [AllAnalAllTheTime] ", score = score, lang = lang))
             Log("....")
+            searchPostNum +=1
         searchPageNum += 1
         Log("")
         Log("--------------------------")
@@ -46,6 +50,16 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
 
 def update(metadata,siteID,movieGenres,movieActors):
     Log('******UPDATE CALLED*******')
+
+    url = str(metadata.id).split("|")[0].replace('+','/').replace('!','?')
+
+    PostNum = url.rsplit("-")[1]
+    Log("updatePostNum:" + PostNum)
+    url = url.rsplit("-")[0]
+    Log("updateURL:" + url)
+    detailsPageElements = HTML.ElementFromURL(url)
+
+    #detailsPageElements = detailsPageElements.xpath('//div[@class="videoarea clear"]')[int(PostNum)]
 
     art = []
     metadata.collections.clear()
@@ -56,10 +70,10 @@ def update(metadata,siteID,movieGenres,movieActors):
     metadata.studio = 'AllAnalAllTheTime'
 
     # Title
-    metadata.title = str(metadata.id).split("|")[0]
-
+    metadata.title = detailsPageElements.xpath('//div[@class="videoarea clear"]/h3/a')[int(PostNum)-1].text_content().strip()
+    
     # Summary
-    metadata.summary = str(metadata.id).split("|")[4]
+    metadata.summary = detailsPageElements.xpath('//div[@class="video_details clear"]/p')[int(PostNum)-1].text_content().split(':')[1][:-15].strip()
 
     # Tagline and Collection(s)
     tagline = PAsearchSites.getSearchSiteName(siteID).strip()
@@ -67,56 +81,22 @@ def update(metadata,siteID,movieGenres,movieActors):
     metadata.collections.add(tagline)
 
     # Genres
-    genres = str(metadata.id).split("|")[3]
-    #if len(genres) > 0:
-    #    for genreLink in genres:
-    #        genreName = genreLink.text_content().strip().lower()
-    #        movieGenres.addGenre(genreName)
+    genres = detailsPageElements.xpath('//div[@class="videoarea clear"]/div/h4/a')[int(PostNum)-1].text_content().split(':')[1].strip()
+    Log("genres: " + genres)
+    if len(genres) > 0:
+        if genres == '100% Anal':
+            genres = "anal"
+        movieGenres.addGenre(genres)
+    movieGenres.addGenre("anal")
 
     # Release Date
-    date = str(metadata.id).split("|")[1]
+    date = detailsPageElements.xpath('//div[@class="videoarea clear"]/p[@class="date"]')[int(PostNum)-1].text_content().strip().replace('th','')
     if len(date) > 0:
-        date_object = datetime.strptime(date, '%B %d, %Y')
+        date_object = datetime.strptime(date, '%B %d %Y')
         metadata.originally_available_at = date_object
         metadata.year = metadata.originally_available_at.year
-
+    
     # Actors
-    actors = str(metadata.id).split("|")[2].split(',')
-    if len(actors) > 0:
-        for actorLink in actors:
-            actorName = actorLink
-            actorPhotoURL = ""
-            movieActors.addActor(actorName,actorPhotoURL)
-
-    ### Posters and artwork ###
-
-    # Video trailer background image
-    try:
-        twitterBG = str(metadata.id).split("|")[5]
-        art.append(twitterBG)
-    except:
-        pass
-
-
-    j = 1
-    Log("Artwork found: " + str(len(art)))
-    for posterUrl in art:
-        if not PAsearchSites.posterAlreadyExists(posterUrl,metadata):            
-            #Download image file for analysis
-            try:
-                img_file = urllib.urlopen(posterUrl)
-                im = StringIO(img_file.read())
-                resized_image = Image.open(im)
-                width, height = resized_image.size
-                #Add the image proxy items to the collection
-                if width > 1 or height > width:
-                    # Item is a poster
-                    metadata.posters[posterUrl] = Proxy.Preview(HTTP.Request(posterUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = j)
-                if width > 100 and width > height:
-                    # Item is an art item
-                    metadata.art[posterUrl] = Proxy.Preview(HTTP.Request(posterUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = j)
-                j = j + 1
-            except:
-                pass
-
+    movieActors.addActor(detailsPageElements.xpath('//div[@class="video_details clear"]/p/span')[int(PostNum)-1].text_content().strip(),"")
+            
     return metadata
